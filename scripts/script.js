9d5cf9e45e205f9d8025eba9d5b829c87b6b54a8
Andrew Jensen <AJensen@student.neumont.edu>
2017-03-08 16:01:50 -0700

    Added form validation

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
 function submitButtonOnPress(){
-	window.alert("Thanks for your comment! We will get back to you as soon as we can!");
+	
+	
+	var nameTextField = document.getElementById('name').value;
+	var emailTextField = document.getElementById('email').value;
+	var commentTextArea = document.getElementById('comment').value;
+	
+	if(nameTextField.length > 0 && emailTextField.length > 0 && commentTextArea.length > 0){
+		window.alert("Thanks for your comment! We will get back to you as soon as we can!");
+	}
 }
